3b93d193cf8324df0b26aa3657e5a794ae3bfb0e
jeremy <je.gautier137@gmail.com>
2020-03-11 13:36:42 +0100

    a propos fini

--- app/src/main/java/com/ivann/kezaco/AboutActivity.java
+++ app/src/main/java/com/ivann/kezaco/AboutActivity.java
@@ -3,6 +3,7 @@ package com.ivann.kezaco;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+
+        TextView tv = findViewById(R.id.versionTextView);
+        tv.setText(BuildConfig.VERSION_NAME);
     }
 }
